4bdfe39b4c05af7e5e9cc54d44a3739a9cfd59db
carter-123 <wilsonc2@middleton.school.nz>
2023-05-10 11:45:20 +1200

    update

--- C3_establish_quiz_v2.py
+++ C3_establish_quiz_v2.py
@@ -1,5 +1,5 @@
-""" Component 3 - Establish quiz v1
-Generate a random maori number from a list
+""" Component 3 - Establish quiz v2 - based on v1
+Put in rounds and a round counter
 """
 import random
 rounds = 0
@@ -7,8 +7,9 @@ rounds = 0
 numbers = ["Tahi", "Rua", "Toru", "Wha", "Rima", "Ono", "Whitu", "Waru", "Iwa", "Tekau"]
 
 
-while rounds >= 10:
+while rounds < 10:
     # Picking random number from the list
-    for item in range(10):
+    for item in range(1):
         number = random.choice(numbers)
-        print(number)
+        input(f"What number is {number} in english? ")
+        rounds += 1
